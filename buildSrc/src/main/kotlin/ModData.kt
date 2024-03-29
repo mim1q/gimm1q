6c9489ec3063b783e297003151518b845dd6cf4d
mim1q <mim1q.dev@gmail.com>
2024-03-29 22:09:12 +0100

    version bump

--- buildSrc/src/main/kotlin/ModData.kt
+++ buildSrc/src/main/kotlin/ModData.kt
@@ -1,7 +1,7 @@
 object ModData {
   const val GROUP = "dev.mim1q"
   const val ID = "gimm1q"
-  const val VERSION = "0.4.1"
+  const val VERSION = "0.4.2"
   const val VERSION_TYPE = "beta"
   val mcVersions = listOf("1.20.1")
   val dependencies = listOf("fabric-api", /*"owo-lib"*/)
