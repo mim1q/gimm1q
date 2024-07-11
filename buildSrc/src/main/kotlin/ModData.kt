9a65fb93ac9e89e43a27efacbefe34ba88d1020f
mim1q <mim1q.dev@gmail.com>
2024-07-11 14:29:11 +0200

    incurable effect bugfix

--- buildSrc/src/main/kotlin/ModData.kt
+++ buildSrc/src/main/kotlin/ModData.kt
@@ -1,7 +1,7 @@
 object ModData {
   const val GROUP = "dev.mim1q"
   const val ID = "gimm1q"
-  const val VERSION = "0.6.0+1.20.1"
+  const val VERSION = "0.6.1+1.20.1"
   const val VERSION_TYPE = "beta"
   val mcVersions = listOf("1.20.1")
   val dependencies = listOf("fabric-api", /*"owo-lib"*/)
